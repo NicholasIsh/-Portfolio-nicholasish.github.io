86f6e428c161762d815c56fd32ef07767a3b545f
NicholasIsherwood <nic.isherwood2001@gmail.com>
2023-06-25 22:16:38 +0200

    Final Changes

--- index.js
+++ index.js
@@ -1,52 +1,36 @@
 window.addEventListener("DOMContentLoaded", function() {
-    var links = document.querySelectorAll('a[href*="#"]');
-
-    for (var i = 0; i < links.length; i++) {
-        links[i].addEventListener('click', function(event) {
-            event.preventDefault();
-
-            var target = document.querySelector(this.getAttribute('href'));
-
-            var offset = target.offsetTop +150 ; 
-
-            
-            window.scrollTo({
-                top: offset,
-                behavior: 'smooth'  
-            });
-        });
-    }
+  var links = Array.from(document.querySelectorAll('a[href*="#"]'));
+  links.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+          event.preventDefault();
+          var target = document.querySelector(this.getAttribute('href'));
+          var offset = target.offsetTop + 110;
+          window.scrollTo({top: offset, behavior:'smooth'});
+      });
+  });
 });
 
 function toggleSortOrder() {
-    var articlesContainer = document.querySelector(".BlogSection");
-    var articles = Array.from(articlesContainer.querySelectorAll("article"));
-  
-    
-    var currentOrder = articlesContainer.getAttribute("data-sort-order");
-    if (currentOrder === "ascending") {
-      articles.reverse();
-      articlesContainer.setAttribute("data-sort-order", "descending");
-      
-      var buttons = document.getElementsByClassName("sortButton");
-
-        for (var i = 0; i < buttons.length; i++) {
-            buttons[i].innerHTML = '<img src="/Images/ascending.png" alt="Sort Descending">';
-        }
-    } else {
-        articles.reverse();
-      articlesContainer.setAttribute("data-sort-order", "ascending");
-      
-      var buttons = document.getElementsByClassName("sortButton");
-
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].innerHTML = '<img src="/Images/descending.png" alt="Sort Descending">';
-}
-    }
-    articlesContainer.innerHTML = "";
-    articles.forEach(function (article) {
-      articlesContainer.appendChild(article);
-    });
+  var articlesContainer = document.querySelector(".BlogSection");
+  var articles = Array.from(articlesContainer.querySelectorAll("article"));
+  var currentOrder = articlesContainer.getAttribute("data-sort-order");
+  if (currentOrder === "ascending") {
+    articles.reverse();
+    articlesContainer.setAttribute("data-sort-order", "descending");
+    var buttons = Array.from(document.getElementsByClassName("sortButton"));
+    buttons.forEach(function(button) {button.innerHTML = '<img src="/Images/ascending.png" alt="Sort Descending">';});
+  } else {
+    articles.reverse();
+    articlesContainer.setAttribute("data-sort-order", "ascending");
+    var buttons = Array.from(document.getElementsByClassName("sortButton"));
+    buttons.forEach(function(button) {button.innerHTML = '<img src="/Images/descending.png" alt="Sort Descending">';});
   }
+  articlesContainer.innerHTML = "";
+  articles.forEach(function(article) {articlesContainer.appendChild(article);});
+}
+
+  function openPage(page) {
+    window.location.href = page;
+}
 
 
